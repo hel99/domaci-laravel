c8da3cd8e59c97758b96bc5cb5a2dd432354848b
hel99 <helena.aganovic99@gmail.com>
2022-08-24 22:26:14 +0200

    api rute i kontroleri za osobu i racun

---
+++ app/Http/Controllers/API/RacunController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Racun;
+use Illuminate\Http\Request;
+
+class RacunController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Racun  $racun
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Racun $racun)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Racun  $racun
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Racun $racun)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Racun  $racun
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Racun $racun)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Racun  $racun
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Racun $racun)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('osoba', OsobaController::class);
+Route::resource('racun', RacunController::class);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
